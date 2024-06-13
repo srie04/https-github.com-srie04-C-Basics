92471aafb6d128735b48e2ba40254a565bab705d
srist <srist@MSI>
2024-06-13 21:59:25 +0530

    Add project files.

---
+++ ImplicitWait.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSharpFundamental_2
+{
+    internal class ImplicitWait
+    {
+        static void Main(string[] args)
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+
+            //Open the Browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Implicit Wait > Global Wait 
+            //Time > 10 seconds 
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
+
+            //maximize
+            driver.Manage().Window.Maximize();
+
+
+            //url
+            driver.Url = "https://www.hyrtutorials.com/p/waits-demo.html";
+
+            driver.FindElement(By.CssSelector("#btn1")).Click();
+
+            bool displayed = driver.FindElement(By.CssSelector("#txt1")).Displayed; //true false
+
+            Console.WriteLine(displayed);
+
+        }
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V123.WebAuthn;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSharpFundamental_2
+{
+    internal class Program2
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Configure the browser
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+
+            //Open the Browser
+            IWebDriver driver = new ChromeDriver();
+
+
+
+            //maximize
+            driver.Manage().Window.Maximize();
+
+
+            //url
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+
+            driver.FindElement(By.CssSelector("input[name = 'username']")).SendKeys("see");
+
+            driver.FindElement(By.CssSelector("#password")).SendKeys("Dash");
+
+            driver.FindElement(By.CssSelector("#terms")).Click();
+
+            //tagname[Attribute = 'value']
+
+            driver.FindElement(By.CssSelector("input[value='Sign In'")).Click(); //Sign In
+
+            Thread.Sleep(3000);
+
+            //Exact Time
+            //Test Fail 
+            //Time Consumption is more
+
+
+           string text = driver.FindElement(By.XPath("//form[@id='login-form']/div/strong/..")).Text;
+
+            Console.WriteLine(text);
+
+
+            driver.Quit();
+
+        }
+       
+        
+    }
+}
